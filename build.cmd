c4eee514cb9da3e5356345fdbeb4279c22dc760a
Ruslan Absalyamov <ruslan.absalyamov@gmail.com>
2016-12-06 00:37:23 +0300

    Fixed build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
-rem del %~dp0dist\*.jar
-rem call mvn clean package
+del %~dp0dist\*.jar
+call mvn clean package
 SET JARFILE=
 FOR %%I in (%~dp0matebot-core\target\matebot-core-*-dependencies.jar) DO SET JARFILE=%%I
 copy %JARFILE% .\dist\matebot.jar /Y
