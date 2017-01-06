b341b28b287a594a16119f1951b5e482459e5b9a
Ruslan Absalyamov <ruslan.absalyamov@gmail.com>
2017-01-06 23:07:30 +0300

    Removed unused code.

--- matebot-web/src/main/java/io/arusland/matebot/web/StartupListener.java
+++ matebot-web/src/main/java/io/arusland/matebot/web/StartupListener.java
@@ -2,21 +2,11 @@ package io.arusland.matebot.web;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class StartupListener implements ServletContextListener {
     private final Logger log = Logger.getLogger(StartupListener.class.getName());
-    private final Timer timer = new Timer();
-
 
     @Override
     public void contextDestroyed(ServletContextEvent ev) {
@@ -28,7 +18,6 @@ public class StartupListener implements ServletContextListener {
         log.info("Bot session starting...");
         try {
             OpenshiftRunner.run();
-            timer.schedule(new RequestTask(), 0, TimeUnit.HOURS.toMillis(1));
         } catch (IOException e) {
             e.printStackTrace();
             log.warning(e.toString());
@@ -36,47 +25,5 @@ public class StartupListener implements ServletContextListener {
             log.warning(e.toString());
             e.printStackTrace();
         }
-    }
-
-    // please, openshift, don't sleep!
-    private class RequestTask extends TimerTask {
-        @Override
-        public void run() {
-            String content = downloadPage("YOUR VALID OPENSHIFT URL");
-            if (!content.isEmpty()) {
-                Statistics.incrementPageViewCount();
-            }
-        }
-    }
-
-    private String downloadPage(String urlLink) {
-        URL url;
-        InputStream is = null;
-        BufferedReader br;
-        String line;
-        StringBuilder sb = new StringBuilder();
-
-        try {
-            url = new URL(urlLink);
-            System.out.println("Downloading link - " + url);
-            is = url.openStream();
-            br = new BufferedReader(new InputStreamReader(is));
-
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-                sb.append("\n");
-            }
-        } catch (MalformedURLException mue) {
-            mue.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } finally {
-            try {
-                if (is != null) is.close();
-            } catch (IOException ioe) {
-            }
-        }
-
-        return sb.toString();
     }
 }
